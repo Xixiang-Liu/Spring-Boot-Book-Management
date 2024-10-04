865d6aa25d8d647fad36ed5e70ef4969ed0c02e2
Xixiang-Liu <xixiangliuoscar@gmail.com>
2024-10-04 17:36:29 -0500

    Set up DAO and let test runtime use h2

---
+++ src/main/java/com/devtiro/database/dao/impl/AuthorDaoImpl.java
@@ -0,0 +1,14 @@
+package com.devtiro.database.dao.impl;
+
+import com.devtiro.database.dao.AuthorDao;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class AuthorDaoImpl implements AuthorDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public AuthorDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+}
